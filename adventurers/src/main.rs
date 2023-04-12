8ed8d27761028cd56252bee5be077cad5d8937e0
sym <yimiao233@outlook.com>
2023-04-13 04:19:04 +1000

    background blocks

--- adventurers/src/main.rs
+++ adventurers/src/main.rs
@@ -1,9 +1,10 @@
-use std::ops::AddAssign;
+use std::collections::HashMap;
+use std::error::Error;
+use std::ops::{Add, AddAssign};
 use std::time::Duration;
-use std::{error::Error, ops::Add};
 use termgame::{
-    run_game, Controller, Game, GameEvent, GameSettings, KeyCode, Message, SimpleEvent,
-    StyledCharacter, ViewportLocation,
+    run_game, Controller, Game, GameColor as Color, GameEvent, GameSettings, GameStyle as Style,
+    KeyCode, Message, SimpleEvent, StyledCharacter, ViewportLocation,
 };
 
 const CHESS_PAWN: char = '♟';
@@ -11,11 +12,51 @@ const CHESS_PAWN: char = '♟';
 /// if distance between player and border < padding, move viewport
 const VIEW_PADDING: i32 = 2;
 
-trait Draw {
-    fn draw(&mut self, game: &mut Game);
+// #[derive(Debug, Clone, Copy)]
+// enum ObjectVariant {
+//     Player,
+//     Background(BackgroundVariant),
+// }
+
+#[derive(Default)]
+struct Place {
+    player: bool,
+    background: Option<BackgroundVariant>,
 }
 
-#[derive(Copy, Clone, Default, Debug)]
+impl Place {
+    fn set_player(&mut self) {
+        self.player = true;
+    }
+    fn remove_player(&mut self) {
+        self.player = false;
+    }
+    fn player(self) -> Self {
+        Self {
+            player: true,
+            ..Default::default()
+        }
+    }
+    fn background(self, background: Option<BackgroundVariant>) -> Self {
+        Self {
+            background: background,
+            ..Default::default()
+        }
+    }
+}
+
+impl Into<StyledCharacter> for &Place {
+    fn into(self) -> StyledCharacter {
+        let mut c = StyledCharacter::new(' ');
+        if self.player {
+            c.c = 'A';
+        }
+        c.style = self.background.map(BackgroundVariant::into);
+        return c;
+    }
+}
+
+#[derive(Copy, Clone, Default, Debug, Eq, Hash, PartialEq)]
 struct Position(i32, i32);
 
 impl Into<ViewportLocation> for Position {
@@ -52,7 +93,7 @@ struct Player {
     update_draw: bool,
     icon: char,
     position: Position,
-    drawn: Option<Position>,
+    previous_position: Option<Position>,
 }
 
 impl Player {
@@ -70,6 +111,51 @@ impl Player {
         self.update_draw = true;
     }
 }
+#[derive(Debug, Clone, Copy)]
+enum BackgroundVariant {
+    Grass,
+    Sand,
+    Rocks,
+    Cinderblock,
+    Flowers,
+}
+
+impl Into<Color> for BackgroundVariant {
+    fn into(self) -> Color {
+        match self {
+            Self::Grass => Color::Green,
+            Self::Sand => Color::LightYellow,
+            Self::Rocks => Color::DarkGray,
+            Self::Cinderblock => Color::LightRed,
+            Self::Flowers => Color::LightMagenta,
+        }
+    }
+}
+
+impl Into<Style> for BackgroundVariant {
+    fn into(self) -> Style {
+        Style::new().background_color(Some(self.into()))
+    }
+}
+
+#[derive(Debug)]
+struct BackgroundBlock {
+    // update_draw: bool,
+    variant: BackgroundVariant,
+    position: Position,
+    previous_position: Option<Position>,
+}
+
+impl BackgroundBlock {
+    fn new(variant: BackgroundVariant, position: Position) -> Self {
+        BackgroundBlock {
+            // update_draw: true,
+            variant,
+            position,
+            previous_position: None,
+        }
+    }
+}
 
 impl Default for Player {
     fn default() -> Self {
@@ -78,23 +164,8 @@ impl Default for Player {
             // icon: PLAYER_CHAR,
             icon: 'A',
             position: Position::default(),
-            drawn: None,
+            previous_position: None,
         }
-    }
-}
-
-impl Draw for Player {
-    fn draw(&mut self, game: &mut Game) {
-        if !self.update_draw {
-            return;
-        }
-        if let Some(Position(x, y)) = self.drawn {
-            game.set_screen_char(x, y, None);
-        }
-        let Position(x, y) = self.position;
-        game.set_screen_char(x, y, Some(StyledCharacter::new(self.icon)));
-        self.drawn = Some(self.position);
-        self.update_draw = false;
     }
 }
 
@@ -113,6 +184,54 @@ impl Control {
 }
 
 #[derive(Default)]
+struct MapPlace {
+    should_draw: Vec<Position>,
+    map: HashMap<Position, Place>,
+}
+
+impl MapPlace {
+    fn update_player(&mut self, player: &mut Player) {
+        if !player.update_draw {
+            return;
+        }
+        if let Some(position) = player.previous_position.take() {
+            if let Some(place) = self.map.get_mut(&position) {
+                place.remove_player();
+            }
+            self.should_draw.push(position);
+        }
+        self.map
+            .entry(player.position)
+            .and_modify(|place| place.set_player())
+            .or_insert_with(|| Place::default().player());
+        player.previous_position = Some(player.position);
+        player.update_draw = false;
+        self.should_draw.push(player.position);
+    }
+    fn update_background(&mut self, background: &mut BackgroundBlock) {
+        if let Some(position) = background.previous_position.take() {
+            if let Some(place) = self.map.get_mut(&position) {
+                place.background = None;
+            }
+            self.should_draw.push(position);
+        }
+        self.map
+            .entry(background.position)
+            .and_modify(|place| place.background = Some(background.variant))
+            .or_insert_with(|| Place::default().background(Some(background.variant)));
+        self.should_draw.push(background.position);
+    }
+
+    fn draw(&mut self, game: &mut Game) {
+        for position in self.should_draw.drain(..) {
+            let Position(x, y) = position;
+            let place = self.map.get(&position);
+            game.set_screen_char(x, y, place.map(<&Place>::into));
+        }
+    }
+}
+
+#[derive(Default)]
 struct MyGame {
     control: Control,
     // test: bool,
@@ -122,9 +241,25 @@ struct MyGame {
     show_text: bool,
     frame: i32,
     player: Player,
+    blocks: Vec<BackgroundBlock>,
+    map_place: MapPlace,
 }
 
 impl MyGame {
+    fn init(&mut self) {
+        self.blocks = vec![
+            BackgroundBlock::new(BackgroundVariant::Grass, Position(5, 6)),
+            BackgroundBlock::new(BackgroundVariant::Sand, Position(6, 8)),
+            BackgroundBlock::new(BackgroundVariant::Sand, Position(6, 9)),
+            BackgroundBlock::new(BackgroundVariant::Rocks, Position(5, 10)),
+            BackgroundBlock::new(BackgroundVariant::Rocks, Position(5, 11)),
+            BackgroundBlock::new(BackgroundVariant::Cinderblock, Position(9, 20)),
+            BackgroundBlock::new(BackgroundVariant::Cinderblock, Position(9, 19)),
+            BackgroundBlock::new(BackgroundVariant::Flowers, Position(10, 20)),
+            BackgroundBlock::new(BackgroundVariant::Flowers, Position(11, 20)),
+        ];
+    }
+
     fn update_player_position(&mut self) {
         if self.control.left {
             self.player.move_by(-1, 0);
@@ -166,22 +301,15 @@ impl MyGame {
     }
 }
 
-impl Draw for MyGame {
-    fn draw(&mut self, game: &mut Game) {
-        self.player.draw(game);
-        game.set_viewport(self.viewport_position.into());
-    }
-}
-
 impl Controller for MyGame {
     fn on_start(&mut self, game: &mut Game) {
         self.player.move_to(3, 3);
         self.viewport_size = game.screen_size();
 
-        game.set_screen_char(5, 6, Some(StyledCharacter::new('a')));
-        game.set_screen_char(20, 16, Some(StyledCharacter::new('b')));
-        game.set_screen_char(23, 10, Some(StyledCharacter::new('c')));
-        game.set_screen_char(12, 14, Some(StyledCharacter::new('d')));
+        self.map_place.update_player(&mut self.player);
+        for b in &mut self.blocks {
+            self.map_place.update_background(b);
+        }
     }
 
     fn on_event(&mut self, game: &mut Game, event: GameEvent) {
@@ -216,13 +344,14 @@ impl Controller for MyGame {
     }
 
     fn on_tick(&mut self, game: &mut Game) {
-
         self.update_player_position();
         self.update_viewport_position();
 
-        self.draw(game);
+        self.map_place.update_player(&mut self.player);
+        self.map_place.draw(game);
 
         self.control.clear();
+        game.set_viewport(self.viewport_position.into());
 
         // let f = format!("{:>8}", self.frame);
         // for (i, ch) in f.chars().enumerate() {
@@ -244,6 +373,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("and try out getting a termgame UI to appear on your terminal.");
 
     let mut controller = MyGame::default();
+    controller.init();
 
     run_game(
         &mut controller,
